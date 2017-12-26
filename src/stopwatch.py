9b1e10f1a6054c475fcd3d4eed21be5874fee9ee
Jkl <jklgit@users.noreply.github.com>
2017-12-26 15:07:31 +0100

    cleaner display

--- src/stopwatch.py
+++ src/stopwatch.py
@@ -9,7 +9,7 @@ while a == '':
     t_current = t.strftime('%H:%M:%S.%f')[:-3]
     dt_0 = sec2hms((t - t_0).total_seconds())
     dt = sec2hms((t - t_old).total_seconds())
-    a = input('%s: %s, %s' % (t_current, dt_0, dt))
+    a = input('%s  |  %s  |  %s' % (t_current, dt_0, dt))
     t_old = t
     t = datetime.datetime.now()
     if a == '0':
